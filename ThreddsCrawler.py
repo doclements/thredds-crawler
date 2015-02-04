fc82f2b59c497632317cb7a814ce00c405816e1e
= <d.o.clements@gmail.com>
2015-02-04 13:56:45 +0000

    some tings

--- ThreddsCrawler.py
+++ ThreddsCrawler.py
@@ -10,67 +10,70 @@ from jinja2 import Environment, FileSystemLoader
 import os
 
 
-
 class ThreddsCrawler():
-   """docstring for ThreddsCrawler"""
-   def __init__(self, url):
-      
-      self.url = url
-      self.namespaces = {
-         'catalog' : 'http://www.unidata.ucar.edu/namespaces/thredds/InvCatalog/v1.0',
-         'xlink'   : 'http://www.w3.org/1999/xlink'
-      }
-      self.top_template = """
+
+    """docstring for ThreddsCrawler"""
+
+    def __init__(self, url):
+
+        self.url = url
+        self.namespaces = {
+            'catalog': 'http://www.unidata.ucar.edu/namespaces/thredds/InvCatalog/v1.0',
+            'xlink': 'http://www.w3.org/1999/xlink'
+        }
+        self.top_template = """
       Name : %(name)s
       services : %(services)s
       catalogs : %(catalogs)s
       """
-      self.catalog = {
-      'base_url' : self.url
-      }
-      self.catalog['services'] = []
-      self.catalog['top_level'] = []
-      self.env = Environment(loader=FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')))
+        self.catalog = {
+            'base_url': self.url
+        }
+        self.catalog['services'] = []
+        self.catalog['top_level'] = []
+        self.env = Environment(
+            loader=FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')))
 
-
-   def getTopLevel(self):
-      xml_catalog = self._fetchXml('/catalog.xml')
-      xml_root = etree.fromstring(xml_catalog)
-      self.catalog['title'] = self._getNode(xml_root,'/catalog:catalog/@name')[0]
-      services = [(x['base'], x['name']) for x in [dict(x.items()) for x in self._getNode(xml_root, '/catalog:catalog/catalog:service/catalog:service')]]
-      for s in services:
-         self.catalog['services'].append({
-            'name' : s[1],
-            'base' : s[0]
+    def getTopLevel(self):
+        xml_catalog = self._fetchXml('/catalog.xml')
+        xml_root = etree.fromstring(xml_catalog)
+        self.catalog['title'] = self._getNode(
+            xml_root, '/catalog:catalog/@name')[0]
+        services = [(x['base'], x['name']) for x in [dict(x.items()) for x in self._getNode(
+            xml_root, '/catalog:catalog/catalog:service/catalog:service')]]
+        for s in services:
+            self.catalog['services'].append({
+                'name': s[1],
+                'base': s[0]
             })
-      catalogs = zip(self._getNode(xml_root ,'/catalog:catalog/catalog:catalogRef/@xlink:href') , self._getNode(xml_root, '/catalog:catalog/catalog:catalogRef/@xlink:title'))
-      catalog_index = 1
-      for c in catalogs:
-         self.catalog['top_level'].append({
-            'id'    : catalog_index,
-            'title' : c[1],
-            'path'  : c[0]
+        catalogs = zip(self._getNode(xml_root, '/catalog:catalog/catalog:catalogRef/@xlink:href'),
+                       self._getNode(xml_root, '/catalog:catalog/catalog:catalogRef/@xlink:title'))
+        catalog_index = 1
+        for c in catalogs:
+            self.catalog['top_level'].append({
+                'id': catalog_index,
+                'title': c[1],
+                'path': c[0]
             })
-         catalog_index = catalog_index + 1
-      
-   def getSub(self,id):
-      choice = [x for x in self.catalog['top_level'] if x['id'] is int(id)][0]
-      print choice['path']
+            catalog_index = catalog_index + 1
 
-      xml = self._fetchXml(choice['path'])
-      return xml
-   def display_catalogs(self):
-      return self.env.get_template('test.template').render(catalog=self.catalog)
+    def getSub(self, id):
+        choice = [
+            x for x in self.catalog['top_level'] if x['id'] is int(id)][0]
+        xml = self._fetchXml(choice['path'])
+        return xml
 
-   def print_datasets(self):
-      return self.top_template % {'name':self.catalog.title, 'services':','.join([x[1] for x in self.services]), 'catalogs': self.catalogs}
+    def display_catalogs(self):
+        return self.env.get_template('test.template').render(catalog=self.catalog)
 
-   def _fetchXml(self, href):
-      doc = urllib2.urlopen(self.url+href)
-      xml = doc.read()
-      doc.close()
-      return xml
+    def print_datasets(self):
+        return self.top_template % {'name': self.catalog.title, 'services': ','.join([x[1] for x in self.services]), 'catalogs': self.catalogs}
 
-   def _getNode(self, root, xpath):
-      return root.xpath(xpath, namespaces=self.namespaces)
-       
+    def _fetchXml(self, href):
+        doc = urllib2.urlopen(self.url + href)
+        xml = doc.read()
+        doc.close()
+        return xml
+
+    def _getNode(self, root, xpath):
+        return root.xpath(xpath, namespaces=self.namespaces)
--- thredds_generator_tool.py
+++ thredds_generator_tool.py
@@ -11,52 +11,65 @@ also allow the generation of HTML and JSON summaries of Thredds offerings"""
 import cmd
 from ThreddsCrawler import ThreddsCrawler
 
+
 class ThreddsExplorer(cmd.Cmd):
-   """docstring for ThreddExplorer"""
-   def __init__(self):
-      cmd.Cmd.__init__(self)
-      self.prompt = "> "
 
-   def do_setthredds(self, url):
-      print "you wanted to explore %s" % url
-      self.t_crawl = ThreddsCrawler(url)
-      self.t_crawl.getTopLevel()
+    """docstring for ThreddExplorer"""
 
-   def do_datasets(self, line):
-      print self.t_crawl.display_catalogs()
+    def __init__(self):
+        cmd.Cmd.__init__(self)
+        self.prompt = "> "
 
-   def do_debug(self, line):
-      print self.t_crawl.catalog
-   def do_q(self, line):
-      return self.do_quit('')
-   def do_template(self, line):
-      print self.t_crawl.t_test()
-   def do_generate(self, line):
-      gen = GenMenu(self.t_crawl)
-      gen.cmdloop()
+    def do_setup(self, url):
+        if len(url) < 1:
+            url = "https://vortices.npm.ac.uk/thredds/"
+        print "you wanted to explore %s" % url
+        self.t_crawl = ThreddsCrawler(url)
+        self.t_crawl.getTopLevel()
 
-   def do_getdataset(self, arg):
-      print arg
-      if int(arg) in [x['id'] for x in self.t_crawl.catalog['top_level']]:
-         print self.t_crawl.getSub(arg)
-      else:
-         print "ERROR : please select a number from the below"
-         self.do_datasets('')
-   def do_quit(self, line):
-      return True
+    def do_catalogs(self, line):
+        print self.t_crawl.display_catalogs()
+
+    def do_debug(self, line):
+        print self.t_crawl.catalog
+
+    def do_q(self, line):
+        return self.do_quit('')
+
+    def do_template(self, line):
+        print self.t_crawl.t_test()
+
+    def do_generate(self, line):
+        gen = GenMenu(self.t_crawl)
+        gen.cmdloop()
+
+    def do_getdataset(self, arg):
+        print arg
+        if int(arg) in [x['id'] for x in self.t_crawl.catalog['top_level']]:
+            print self.t_crawl.getSub(arg)
+        else:
+            print "ERROR : please select a number from the below"
+            self.do_datasets('')
+
+    def do_quit(self, line):
+        return True
+
 
 class GenMenu(cmd.Cmd):
-   """docstring for GenMenu"""
-   def __init__(self, catalog):
-      cmd.Cmd.__init__(self)
-      self.catalog = catalog
-      self.prompt = "GEN > "
 
-   def do_test(self,line):
-      print self.catalog.catalog
-   def do_quit(self, line):
-      return True
-      
+    """docstring for GenMenu"""
+
+    def __init__(self, catalog):
+        cmd.Cmd.__init__(self)
+        self.catalog = catalog
+        self.prompt = "GEN > "
+
+    def do_test(self, line):
+        print self.catalog.catalog
+
+    def do_quit(self, line):
+        return True
+
 
 if __name__ == '__main__':
     ThreddsExplorer().cmdloop()
